03e9ad0c3ae5574f8c3780937eac7e1b29442138
97Fangz <briansegem25@gmail.com>
2024-03-07 11:53:09 +0300

    "c"

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Ac'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Ace97'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
