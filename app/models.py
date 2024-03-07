2decc7fae256b3d6b9a0d7f424bd7263b05414c0
97Fangz <briansegem25@gmail.com>
2024-03-07 11:59:51 +0300

    ":"

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timezone, timedelta
+from hashlib import md5
+import json
+import secrets
+from time import time
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from flask import current_app, url_for
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import redis
+import rq
+from app import db, login
+from app.search import add_to_index, remove_from_index, query_index
